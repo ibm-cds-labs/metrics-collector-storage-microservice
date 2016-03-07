f9db6198c8f145f0ce0daecae9c126b9e804a406
Glynn Bird <glynn.bird@gmail.com>
2016-03-07 12:38:19 +0000

    added simple http server and cfignore

--- app.js
+++ app.js
@@ -9,4 +9,13 @@ var q = require('./plugins/hub/' + queue_type);
 
 q.collect();
 
+var cfenv = require('cfenv'),
+  appEnv = cfenv.getAppEnv(),
+  http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end("I'm alive");
+}).listen(appEnv.port);
+console.log("Listening on", appEnv.port)
+
 require("cf-deployment-tracker-client").track();
