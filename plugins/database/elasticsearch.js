212c268d5e885040b44a9231415f226281c41d7b
Glynn Bird <glynn.bird@gmail.com>
2016-04-07 09:56:00 +0100

    stash work on ES mappings

--- plugins/database/elasticsearch.js
+++ plugins/database/elasticsearch.js
@@ -2,6 +2,114 @@ var elasticsearch = require('elasticsearch');
 var client = null;
 var DB_NAME = process.env.DB_NAME || "mc";
 
+/*var mappings = {
+  mappings: {
+    mc: {
+      properties: {
+        e_c : {
+          type: "string",
+          index: "not_analyzed" 
+        },
+        e_a : {
+          type: "string",
+          index: "not_analyzed" 
+        },
+        idsite : {
+          type: "string",
+          index: "not_analyzed" 
+        },
+        rec : {
+          type: "long"
+        },
+        r : {
+          type: "long"
+        },
+        h : {
+          type: "long"
+        },
+        m : {
+          type: "long"
+        },
+        s : {
+          type: "long"
+        },
+        url : {
+          type: "string"
+        },
+        id : {
+          type: "string",
+          index: "not_analyzed" 
+        },
+        idts : {
+          type: "date",
+          format: "epoch_second"
+        },
+        idvc : {
+          type: "long"
+        },
+        idvn : {
+          type: "long"
+        },
+        refts : {
+          type: "long"
+        },
+        viewts : {
+          type: "date",
+          format: "epoch_second"
+        },
+        cs : {
+          type: "string",
+          index: "not_analyzed" 
+        },
+        send_image : {
+          type: "long"
+        },
+        pdf : {
+          type: "long"
+        },
+        qt : {
+          type: "long"
+        },
+        realp : {
+          type: "long"
+        },
+        wma : {
+          type: "long"
+        },
+        dir : {
+          type: "long"
+        },
+        fla : {
+          type: "long"
+        },
+        java : {
+          type: "long"
+        },
+        gears : {
+          type: "long"
+        },
+        ag : {
+          type: "long"
+        },
+        cookie : {
+          type: "long"
+        },
+        res : {
+          type: "string",
+          index: "not_analyzed" 
+        },
+        gt_ms : {
+          type: "long"
+        },
+        ip : {
+          type: "string",
+          index: "not_analyzed" 
+        }
+      }
+    }
+  }
+};*/
+
 
 var connect = function(callback) {
   var credentials = require('../../lib/bmservice').getCredentials(/^Elasticsearch by Compose/) || { public_hostname: "localhost/9200", username: "", password: ""};
